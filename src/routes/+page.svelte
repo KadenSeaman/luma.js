48ccdd8fb6281911ceea6b890cc54e1b4083e6ee
KadenSeaman <kadencs24@outlook.com>
2024-11-27 20:49:05 -0700

    draggable background

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,4 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+import LumaRenderer from './LumaRenderer.svelte';
+</script>
+<LumaRenderer grid=true width=1080px height=1000px backgroundColor=white></LumaRenderer>
